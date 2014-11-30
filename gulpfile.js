e8e5c5d89d4cec512f7de7339de9a8fb1171407b
Alexis Mineaud <alexis.mineaud@gmail.com>
2014-12-01 00:20:25 +0100

    Add generation of font.

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,10 @@ var gulp        = require('gulp'),
     clean       = require('gulp-clean'),
     less        = require('gulp-less'),
     sourcemaps  = require('gulp-sourcemaps'),
-    minifyCss   = require('gulp-minify-css');
-
+    // minifyCss   = require('gulp-minify-css'),
+    iconfont    = require('gulp-iconfont'),
+    consolidate = require('gulp-consolidate'),
+    rename      = require('gulp-rename');
 
 
 gulp.task('_clean', function () {
@@ -15,8 +17,27 @@ gulp.task('_clean', function () {
     .pipe(clean());
 });
 
+gulp.task('_build-font', ['_clean'], function () {
+  gulp.src('./svg/*.svg')
+    .pipe(iconfont({
+      fontName: 'cr0cK',
+      normalize: true
+    }))
+    .on('codepoints', function (codepoints) {
+      gulp.src('templates/font.css')
+        .pipe(consolidate('lodash', {
+          glyphs: codepoints,
+          fontName: 'cr0cK',
+          fontPath: '/fonts/',
+          className: 'icon'
+        }))
+        .pipe(rename('font.less'))
+        .pipe(gulp.dest('./less/'));
+    })
+    .pipe(gulp.dest('www/fonts/'));
+});
 
-gulp.task('_build-less', ['_clean'],function () {
+gulp.task('_build-less', ['_clean'], function () {
   gulp
     .src('./less/main.less')
     .pipe(sourcemaps.init())
@@ -25,13 +46,14 @@ gulp.task('_build-less', ['_clean'],function () {
     .pipe(gulp.dest('./www/css'));
 });
 
-
-gulp.task('_watch', function () {
+gulp.task('_watch', ['_build-less'], function () {
   gulp.watch([
-    './less/main.less'
-  ], ['_build']);
+    './less/**/*.less'
+  ], ['_build-less']);
 });
 
-
-gulp.task('build', ['_build-less']);
 gulp.task('watch', ['_watch']);
+gulp.task('build:less', ['_build-less']);
+gulp.task('build:font', ['_build-font']);
+
+
