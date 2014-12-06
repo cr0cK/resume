4fb911f4d20e29a1c127b3fc50f41c2086d45dde
Alexis Mineaud <alexis.mineaud@gmail.com>
2014-12-06 02:03:17 +0100

    Minify css and html.

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@
 var gulp        = require('gulp'),
     clean       = require('gulp-clean'),
     less        = require('gulp-less'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    // minifyCss   = require('gulp-minify-css'),
+    minifyCss   = require('gulp-minify-css'),
+    minifyHtml  = require('gulp-minify-html'),
     iconfont    = require('gulp-iconfont'),
     consolidate = require('gulp-consolidate'),
     rename      = require('gulp-rename');
@@ -40,19 +40,25 @@ gulp.task('_build-font', ['_clean'], function () {
 gulp.task('_build-less', ['_clean'], function () {
   gulp
     .src('./less/main.less')
-    .pipe(sourcemaps.init())
     .pipe(less())
-    .pipe(sourcemaps.write('./maps'))
+    .pipe(minifyCss())
     .pipe(gulp.dest('./www/css'));
 });
 
-gulp.task('_watch', ['_build-less'], function () {
-  gulp.watch([
-    './less/**/*.less'
-  ], ['_build-less']);
+gulp.task('_build-html', ['_clean'], function () {
+  gulp
+    .src('./html/*.html')
+    .pipe(minifyHtml())
+    .pipe(gulp.dest('./www'));
+});
+
+gulp.task('_watch', ['_build-less', '_build-html'], function () {
+  gulp.watch('./less/**/*.less', ['_build-less']);
+  gulp.watch('./html/*.html', ['_build-html']);
 });
 
 gulp.task('watch', ['_watch']);
+gulp.task('build:all', ['_build-less', '_build-html']);
 gulp.task('build:less', ['_build-less']);
 gulp.task('build:font', ['_build-font']);
 
